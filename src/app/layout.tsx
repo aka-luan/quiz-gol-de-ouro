a6451be7156e343d049a3c88a12d9283fa0c0ea2
Luan Alves <akaluan.la@gmail.com>
2025-10-27 01:02:04 -0300

    base layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='pt-br'>
-      <body>{children}</body>
+      <body>
+        <div>
+          <nav className='flex font-bold w-full px-10 py-4'>
+            <p className='font-bold text-lg'>⚽ Quiz Bola de Ouro</p>
+            <ul className='flex gap-4 ml-auto'>
+              <li>
+                <a href='/'>Início</a>
+              </li>
+              <li>
+                <a href='/quiz'>Jogar</a>
+              </li>
+              <li>
+                <a href='/ranking'>Ranking</a>
+              </li>
+              <li>
+                <a href='/conquistas'>Conquistas</a>
+              </li>
+            </ul>
+          </nav>
+          <div className='max-w-3xl mx-auto p-4'>{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
