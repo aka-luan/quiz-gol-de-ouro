bb033be42f9b390eef496f22ebf9f23455296436
Luan Alves <lfalves.terc@pa.sebrae.com.br>
2025-10-28 16:49:55 -0300

    mobile first homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/Header';
 import './globals.css';
 
 export default function RootLayout({
@@ -9,24 +10,8 @@ export default function RootLayout({
     <html lang='pt-br'>
       <body>
         <div>
-          <nav className='flex font-bold w-full px-10 py-4'>
-            <p className='font-bold text-lg'>⚽ Quiz Bola de Ouro</p>
-            <ul className='flex gap-4 ml-auto'>
-              <li>
-                <a href='/'>Início</a>
-              </li>
-              <li>
-                <a href='/quiz'>Jogar</a>
-              </li>
-              <li>
-                <a href='/ranking'>Ranking</a>
-              </li>
-              <li>
-                <a href='/conquistas'>Conquistas</a>
-              </li>
-            </ul>
-          </nav>
-          <div className='max-w-3xl mx-auto p-4'>{children}</div>
+          <Header />
+          <div className='mx-auto p-4'>{children}</div>
         </div>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,31 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 'use client';
 import Button from '@/components/Button';
+import { Star, Trophy, ArrowUp, Flame, Gamepad2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 const page = () => {
   const router = useRouter();
 
   return (
-    <main className='flex flex-col items-center justify-center gap-6'>
-      <h1 className='text-3xl'>Bem-vindo ao Quiz Gol de Ouro</h1>
-      <p>
-        Teste seus conhecimentos sobre futebol e conquiste o topo do ranking!
+    <main className='flex flex-col items-center justify-center gap-6 bg-blue-600 py-10 px-6 rounded-4xl border border-cyan-400 shadow-lg shadow- shadow-amber-50 max-w-xl mx-auto'>
+      <div className='flex gap-6 items-center justify-center'>
+    <Trophy className='w-20 h-20'/>
+        <h1 className='text-4xl font-bold'>Quiz Gol <br /> de Ouro</h1>
+      </div>
+      <p className='text-xl'>
+        Teste seus conhecimentos sobre futebol, ganhe XP, suba de nível e desbloqueie conquista!
       </p>
-      <Button variant='primary' onClick={() => router.push('/quiz')}>
-        Jogar agora
+      <Button className="text-xl" variant='primary' onClick={() => router.push('/quiz')}>
+        Jogar Agora
       </Button>
+      <div className='w-full p-4 rounded-2xl bg-blue-500'>
+        <ul className='flex flex-col text-xl gap-2 font-medium'>
+          <li className='flex gap-2'><Star fill="yellow" color='yellow'/>Nível: Craque</li>
+          <li className='flex gap-2'><ArrowUp fill="yellow" color='yellow' strokeWidth={3}/>XP Total: 620</li>
+          <li className='flex gap-2'><Flame />Maior Sequência: 8</li>
+          <li className='flex gap-2'><Gamepad2 />Melhor Partida: 9/10</li>
+        </ul>
+      </div>
     </main>
   );
 };
