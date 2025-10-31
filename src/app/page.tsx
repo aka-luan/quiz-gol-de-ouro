54e9b14a8032fd24aec4ca8b1acea791fb491520
Luan Alves <lfalves.terc@pa.sebrae.com.br>
2025-10-31 17:23:13 -0300

    added time and streak to quiz page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,59 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-'use client';
-import Button from '@/components/Button';
-import { Star, Trophy, ArrowUp, Flame, Gamepad2, CirclePlay } from 'lucide-react';
-import { useRouter } from 'next/navigation';
+"use client"
+import Button from "@/components/Button"
+import {
+  Star,
+  Trophy,
+  ArrowUp,
+  Flame,
+  Gamepad2,
+  CirclePlay,
+} from "lucide-react"
+import { useRouter } from "next/navigation"
+
 const page = () => {
-  const router = useRouter();
+  const router = useRouter()
 
   return (
-    <main className='flex flex-col items-center justify-center gap-8 bg-deep pt-16 pb-10 px-8 rounded-3xl border border-surface-1 shadow-card max-w-xl mx-auto'>
-      <div className='flex gap-4 items-center justify-center'>
-    <Trophy className='w-22 h-22 text-accent-amber-500'/>
-        <h1 className='text-5xl font-medium font-display'>Quiz Gol <br /> de Ouro</h1>
+    <main className="flex flex-col items-center justify-center gap-8 bg-deep pt-16 pb-10 px-8 rounded-3xl border border-surface-1 shadow-card max-w-xl mx-auto">
+      <div className="flex gap-4 items-center justify-center">
+        <Trophy className="w-22 h-22 text-accent-amber-500" />
+        <h1 className="text-5xl font-medium font-display">
+          Quiz Gol <br /> de Ouro
+        </h1>
       </div>
-      <p className='text-xl'>
-        Teste seus conhecimentos sobre futebol, ganhe XP, suba de nível e desbloqueie conquista!
+      <p className="text-xl">
+        Teste seus conhecimentos sobre futebol, ganhe XP, suba de nível e
+        desbloqueie conquista!
       </p>
-      <Button className="text-xl bg-gradient-button font-medium flex items-center justify-center gap-2 shadow-elevated hover:scale-[1.02] active:scale-[0.98] transition" onClick={() => router.push('/quiz')}>
-        <CirclePlay />Jogar Agora
+      <Button
+        className="text-xl bg-gradient-button font-medium flex items-center justify-center gap-2 shadow-elevated hover:scale-[1.02] active:scale-[0.98] transition"
+        onClick={() => router.push("/quiz")}>
+        <CirclePlay />
+        Jogar Agora
       </Button>
-      <div className='w-full p-4 rounded-2xl bg-surface-1 card-surface shadow-elevated border border-surface-2'>
-        <ul className='flex flex-col text-xl gap-2 font-mono'>
-          <li className='flex gap-3'><Star color='#E9B23E' />Nível: Craque</li>
-          <li className='flex gap-3'><ArrowUp  color='#E9B23E' strokeWidth={2}/>XP Total: 620</li>
-          <li className='flex gap-3'><Flame  color='#E9B23E' />Maior Sequência: 8</li>
-          <li className='flex gap-3'><Gamepad2  color='#E9B23E' />Melhor Partida: 9/10</li>
+      <div className="w-full p-4 rounded-2xl bg-surface-1 card-surface shadow-elevated border border-surface-2">
+        <ul className="flex flex-col text-xl gap-2 font-mono">
+          <li className="flex gap-3">
+            <Star color="#E9B23E" />
+            Nível: Craque
+          </li>
+          <li className="flex gap-3">
+            <ArrowUp color="#E9B23E" strokeWidth={2} />
+            XP Total: 620
+          </li>
+          <li className="flex gap-3">
+            <Flame color="#E9B23E" />
+            Maior Sequência: 8
+          </li>
+          <li className="flex gap-3">
+            <Gamepad2 color="#E9B23E" />
+            Melhor Partida: 9/10
+          </li>
         </ul>
       </div>
-      
     </main>
-  );
-};
+  )
+}
 
-export default page;
+export default page
--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,28 +1,52 @@
 'use client';
 import Button from "@/components/Button"
 import Progress from "@/components/Progress"
+import { Clock } from "lucide-react";
 function quiz() {
   return (
-    <main className='flex flex-col items-center justify-center gap-6 bg-surface-1 pt-16 pb-10 px-8 rounded-3xl border border-surface-2 shadow-card max-w-xl mx-auto'>
+    <main className="flex flex-col items-center justify-center gap-6 bg-surface-1 pt-16 pb-10 px-8 rounded-3xl border border-surface-2 shadow-card max-w-xl mx-auto">
       <div className="w-full border-b border-surface-2 pb-4">
         <div className="flex justify-between mb-2">
           <p>Perguntas 3 / 10</p>
           <p>XP: 80</p>
+          <p>Streak: 4</p>
         </div>
         <Progress max={10} value={3} />
       </div>
       <div className="flex flex-col gap-6 w-full">
         <div className="flex justify-between">
-          <p>ICONE Copa do Mundo</p> 
+          <p>ICONE Copa do Mundo</p>
           <p>Fácil</p>
         </div>
       </div>
-      <h2 className="text-4xl font-medium font-display mb-2">Quem venceu a copa do mundo de 2018?</h2>
+      <h2 className="text-4xl font-medium font-display mb-2">
+        Quem venceu a copa do mundo de 2018?
+      </h2>
       <div className="flex flex-col gap-2 w-full">
-        <Button className="text-xl font-medium text-left px-6 bg-gradient-right-answer border-surface-2 text-text-primary" onClick={() => {}}>França</Button>
-        <Button className="text-xl font-medium text-left px-6 bg-deep border-surface-1 text-text-primary" onClick={() => {}}>Bélgica</Button>
-        <Button className="text-xl font-medium text-left px-6 bg-deep border-surface-1 text-text-primary" onClick={() => {}}>Inglaterra</Button>
-        <Button className="text-xl font-medium text-left px-6 bg-gradient-wrong-answer border-surface-2 text-text-primary" onClick={() => {}}>Croácia</Button>
+        <Button
+          className="text-xl font-medium text-left px-6 bg-gradient-right-answer border-surface-2 text-text-primary"
+          onClick={() => {}}>
+          França
+        </Button>
+        <Button
+          className="text-xl font-medium text-left px-6 bg-deep border-surface-1 text-text-primary"
+          onClick={() => {}}>
+          Bélgica
+        </Button>
+        <Button
+          className="text-xl font-medium text-left px-6 bg-deep border-surface-1 text-text-primary"
+          onClick={() => {}}>
+          Inglaterra
+        </Button>
+        <Button
+          className="text-xl font-medium text-left px-6 bg-gradient-wrong-answer border-surface-2 text-text-primary"
+          onClick={() => {}}>
+          Croácia
+        </Button>
+      </div>
+      <div className="flex gap-2">
+        <Clock />
+        <p>Tempo: 15s</p>
       </div>
     </main>
   )
